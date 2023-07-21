da986f2942a28d41ef568d0eb54e0a5228de0d45
Hocine Benouddane <hocine20992@gmail.com>
2023-07-21 10:19:22 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+let num1 = 10;
+let num2 = 6.2;
+let multiply = num1 * num2;
+
+let random = parseInt(Math.random() + 1);
+
+let num3 = 19;
+let num4 = 5;
+let mod = num3 % num4;
+
+let max = Math.max(2, 8, 5, 11, 20, 3, 18);
